3821e61089daf94dd8eea5face9872116251357c
Andrew Jones <andrewrayjones@gmail.com>
2021-04-19 16:19:58 -0500

    create EFS
    Co-authored-by: Melinda Lim <melinda.hl.lim@gmail.com>
    Co-authored-by: Sheila Babadi <sheila.babadi@yahoo.com>
    Co-authored-by: Chris Durand <chris@cdurand.net>

---
+++ src/util/prompts/efsCreate.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line unicorn/filename-case
+const inquirer = require('inquirer')
+
+const efsCreatePrompt = async () => {
+  let response = await inquirer.prompt([{
+    name: 'efsName',
+    message: 'What would you like to call the Elastic File System?',
+    type: 'input',
+  }])
+
+  return response
+}
+
+module.exports = efsCreatePrompt
