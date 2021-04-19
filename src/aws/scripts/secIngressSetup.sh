c143830cad83f163c6f285bc4f9e1c555fa10458
Melinda <melinda.hl.lim@gmail.com>
2021-04-19 16:07:34 -0400

    Add create security group for EFS - untested
    Co-authored-by: Andrew Jones <andrewrayjones@gmail.com>
    Co-authored-by: Sheila Babadi <sheila.babadi@yahoo.com>
    Co-authored-by: Chris Durand <chris@cdurand.net>

--- src/aws/scripts/secIngressSetup.sh
+++ src/aws/scripts/secIngressSetup.sh
@@ -2,26 +2,6 @@
 
 # sudo apt-get install jq
 
-function getJsonValue()
-{
-  # requires jq to be installed
-  # first arg: path/filename that holds the JSON object
-  # second arg: field name (or "path") within the JSON object
-  # third arg: variable name that will store the result value
-
-  # store result variable name in a local variable
-  local __resultvar=$3
-  
-  # grab the JSON value
-  local jsonvalue=$(jq $2 $1)
-
-  # remove quotes from retrieved JSON value
-  jsonvalue="${jsonvalue%\"}"
-  jsonvalue="${jsonvalue#\"}"
-  
-  eval $__resultvar="'$jsonvalue'"
-}
-
 # aws ec2 create-key-pair \
 # --key-name $AWS_KEY_NAME \
 # --query 'KeyMaterial' \
