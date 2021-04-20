e26adbdadb6e02073c5fb908d41043ea774720fa
Melinda <melinda.hl.lim@gmail.com>
2021-04-20 08:42:12 -0400

    Refactor output display for cleaner invocation

--- src/commands/setup.js
+++ src/commands/setup.js
@@ -1,8 +1,7 @@
 const {Command} = require('@oclif/command')
 const api = require('../aws/api')
 const prompts = require('../prompts')
-
-const sleep = require('../util')
+const utils = require('../util')
 
 class SetupCommand extends Command {
   async run() {
@@ -19,60 +18,51 @@ class SetupCommand extends Command {
     // create vpc
     const vpcCreateResponse = await prompts.createVpc()
     const {awsVpcCreateResponse, vpcCreateError} = await api.createVpc(vpcCreateResponse)
-    console.log('awsVpcCreateResponse: ', awsVpcCreateResponse)
-    console.log('vpcCreateError: ', vpcCreateError)
+    utils.display(awsVpcCreateResponse, vpcCreateError)
 
     const vpcId = JSON.parse(awsVpcCreateResponse).Vpc.VpcId
 
     // create security groups and rules
     const secGroupResponse = await prompts.createSecurityGroup()
     const {awsSecGroupResponse, secGroupError} = await api.createSecurityGroup(vpcId, secGroupResponse)
-    console.log('awsSecGroupResponse: ', awsSecGroupResponse)
-    console.log('secGroupError: ', secGroupError)
+    utils.display(awsSecGroupResponse, secGroupError)
 
     const secGroupId = JSON.parse(awsSecGroupResponse).GroupId
     const {awsSGIngressResponse, SGIngressError} = await api.setSgIngress(secGroupId)
-    console.log('awsSGIngressResponse: ', awsSGIngressResponse)
-    console.log('SGIngressError: ', SGIngressError)
+    utils.display(awsSGIngressResponse, SGIngressError)
 
     // create subnet
     const subnetCreateResponse = await prompts.createSubnet()
     const {awsSubnetCreateResponse, subnetCreateError} = await api.createSubnet(vpcId, subnetCreateResponse)
-    console.log('awsSubnetCreateResponse: ', awsSubnetCreateResponse)
-    console.log('subnetCreateError: ', subnetCreateError)
+    utils.display(awsSubnetCreateResponse, subnetCreateError)
 
     const subnetId = JSON.parse(awsSubnetCreateResponse).Subnet.SubnetId
 
     // create internet gateway
     const igCreateResponse = await prompts.createInternetGateway()
     const {awsIgCreateResponse, igCreateError} = await api.createInternetGateway(igCreateResponse)
-    console.log('awsIgCreateResponse: ', awsIgCreateResponse)
-    console.log('igCreateError: ', igCreateError)
+    utils.display(awsIgCreateResponse, igCreateError)
 
     const igId = JSON.parse(awsIgCreateResponse).InternetGateway.InternetGatewayId
 
     // attach internet gateway
     const {awsIgAttachResponse, igAttachError} = await api.attachInternetGateway(igId, vpcId)
-    console.log('awsIgAttachResponse: ', awsIgAttachResponse)
-    console.log('igAttachError: ', igAttachError)
+    utils.display(awsIgAttachResponse, igAttachError)
 
     // create route table
     const routeTableCreateResponse = await prompts.createRouteTable()
     const {awsRouteTableCreateResponse, routeTableCreateError} = await api.createRouteTable(vpcId, routeTableCreateResponse)
-    console.log('awsRouteTableCreateResponse: ', awsRouteTableCreateResponse)
-    console.log('routeTableCreateError: ', routeTableCreateError)
+    utils.display(awsRouteTableCreateResponse, routeTableCreateError)
 
     const routeTableId = JSON.parse(awsRouteTableCreateResponse).RouteTable.RouteTableId
 
     // create route
     const {awsRouteCreateResponse, routeCreateError} = api.createRoute(routeTableId, igId)
-    console.log('awsRouteCreateResponse: ', awsRouteCreateResponse)
-    console.log('routeCreateError: ', routeCreateError)
+    utils.display(awsRouteCreateResponse, routeCreateError)
 
     // associate route table
     const {awsRouteTableAssociateResponse, routeTableAssociateError} = api.associateRouteTable(routeTableId, subnetId)
-    console.log('awsRouteTableAssociateResponse: ', awsRouteTableAssociateResponse)
-    console.log('routeTableAssociateError: ', routeTableAssociateError)
+    utils.display(awsRouteTableAssociateResponse, routeTableAssociateError)
 
     // ~~~ Security group stuff for EFS ~~~ //
 
@@ -87,19 +77,16 @@ class SetupCommand extends Command {
 
     // authorize EFS security group to receive (ingress) traffic from VPC
     const {awsEfsSgIngressResponse, efsSgIngressError} = await api.setEfsSgIngress(efsSgId, secGroupId)
-    console.log('awsEfsSgIngressResponse :', awsEfsSgIngressResponse)
-    console.log('efsSgIngressError :', efsSgIngressError)
+    utils.display(awsEfsSgIngressResponse, efsSgIngressError)
 
     // authorize VPC security group to send out (egress) traffic to EFS
     const {awsEfsSgEgressResponse, efsSgEgressError} = await api.setEfsSgEgress(efsSgId, secGroupId)
-    console.log('awsEfsSgEgressResponse :', awsEfsSgEgressResponse)
-    console.log('efsSgEgressError :', efsSgEgressError)
+    utils.display(awsEfsSgEgressResponse, efsSgEgressError)
 
     // create EFS
     const efsName = await prompts.createEfs()
     const {awsEfsCreateResponse, efsCreateError} = await api.createEfs(efsName)
-    console.log('awsEfsCreateResponse :', awsEfsCreateResponse)
-    console.log('efsCreateError :', efsCreateError)
+    utils.display(awsEfsCreateResponse, efsCreateError)
 
     const efsId = JSON.parse(awsEfsCreateResponse).FileSystemId
 
@@ -109,15 +96,14 @@ class SetupCommand extends Command {
     let efsDescribeError
 
     while (efsState !== 'available' && !efsDescribeError) {
-      sleep(500)
+      utils.sleep(500)
 
       // eslint-disable-next-line no-await-in-loop
       const response = await api.describeFileSystem(efsId)
       awsEfsDescribeResponse = response.awsEfsDescribeResponse
       efsDescribeError = response.efsDescribeError
 
-      console.log('awsEfsDescribeResponse: ', awsEfsDescribeResponse)
-      console.log('efsDescribeError: ', efsDescribeError)
+      utils.display(awsEfsDescribeResponse, efsDescribeError)
 
       efsState = JSON.parse(awsEfsDescribeResponse).FileSystems[0].LifeCycleState
     }
@@ -125,8 +111,7 @@ class SetupCommand extends Command {
     // create mount target in subnet -- make sure to add it to EFS security group
     // TODO: EFS DNS can't be resolved when we try to launch
     const {awsMountTargetResponse, mountTargetError} = await api.createMountTarget(efsId, subnetId, efsSgId)
-    console.log('awsMountTargetResponse :', awsMountTargetResponse)
-    console.log('mountTargetError :', mountTargetError)
+    utils.display(awsMountTargetResponse, mountTargetError)
 
     const mountTargetId = JSON.parse(awsMountTargetResponse).MountTargetId
   }
