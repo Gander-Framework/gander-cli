b31b3730c05398b3851f38d22ed6e2a192b2d424
sjbabadi <sheila.babadi@yahoo.com>
2021-04-21 13:46:36 -0400

    commit changes to checkout setup
    Co-authored-by: Melinda Lim <melinda.hl.lim@gmail.com>
    Co-authored-by: Andrew Jones <andrewrayjones@gmail.com>
    Co-authored-by: Chris Durand <chris@cdurand.net>

--- src/commands/init.js
+++ src/commands/init.js
@@ -1,10 +1,14 @@
 const { Command } =  require('@oclif/command')
 const prompts = require('../prompts')
 const utils = require('../util')
+const path = require('path');
 
 
 class InitCommand extends Command {
-
+  async run() {
+    console.log(path.join(__dirname, "/../.."));  
+    console.log(process.cwd())
+  }
 }
 
 InitCommand.description = 'Initialize a Fleet App in your project repository'
