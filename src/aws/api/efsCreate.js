1d7f7ec4485b04271d4815d59b1ab8bebe600202
Melinda <melinda.hl.lim@gmail.com>
2021-04-21 09:27:12 -0400

    Remove hardcoded values from create EFS script

--- src/aws/api/efsCreate.js
+++ src/aws/api/efsCreate.js
@@ -3,12 +3,14 @@ const path = require('path')
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 
-const efsCreateCall = async response => {
+const efsCreateCall = async (region, response) => {
   const script = path.resolve(__dirname, '../scripts/efsCreate.sh')
   const arg1 = `EFS_NAME=${response.name}`
+  const arg2 = `REGION=${region}`
+  const arg3 = `CREATION_TOKEN=${response.creationToken}`
 
   try {
-    const {stdout, stderr} = await exec(`${arg1} ${script}`)
+    const {stdout, stderr} = await exec(`${arg1} ${arg2} ${arg3} ${script}`)
     return {awsEfsCreateResponse: stdout, efsCreateError: {awsError: stderr}}
   } catch (error) {
     return {awsEfsCreateResponse: {}, efsCreateError: error}
