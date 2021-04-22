2c1e6715ee8a5110eabdba85ecbf70e0369f3c85
Melinda <melinda.hl.lim@gmail.com>
2021-04-22 14:07:16 -0400

    Add creating an ECR repository in setup command; change permissions and invocations for creating cluster
    Co-authored-by: Andrew Jones <andrewrayjones@gmail.com>
    Co-authored-by: Sheila Babadi <sheila.babadi@yahoo.com>
    Co-authored-by: Chris Durand <chris@cdurand.net>

--- src/util/paths.js
+++ src/util/paths.js
@@ -1,9 +1,9 @@
-const path = require("path")
+const path = require('path')
 
-const fleetRootPath = path.join(__dirname, "/../..") // ends up at root level of fleet-cli
+const fleetRootPath = path.join(__dirname, '/../..') // ends up at root level of fleet-cli
 
-const fleetBuildWorkflowTemplatePath = path.join(fleetRootPath, "/templates/build-review-app.yml");
-const userBuildWorkflowPath = path.join(process.cwd(), "/.github/workflows/build-review-app.yml");
+const fleetBuildWorkflowTemplatePath = path.join(fleetRootPath, '/templates/build-review-app.yml')
+const userBuildWorkflowPath = path.join(process.cwd(), '/.github/workflows/build-review-app.yml')
 
 const githubFolderPath = path.join(process.cwd(), '/.github')
 const workflowFolderPath = path.join(process.cwd(), '/.github/workflows')
