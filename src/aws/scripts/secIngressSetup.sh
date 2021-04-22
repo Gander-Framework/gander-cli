953232071b7d1cc2ae906a9bd3883def743f5b11
Melinda <melinda.hl.lim@gmail.com>
2021-04-22 13:50:20 -0400

    Add ingress rule for 8080 on cluster security group
    Co-authored-by: Andrew Jones <andrewrayjones@gmail.com>
    Co-authored-by: Sheila Babadi <sheila.babadi@yahoo.com>
    Co-authored-by: Chris Durand <chris@cdurand.net>

--- src/aws/scripts/secIngressSetup.sh
+++ src/aws/scripts/secIngressSetup.sh
@@ -28,6 +28,12 @@ aws ec2 authorize-security-group-ingress \
 
 aws ec2 authorize-security-group-ingress \
 --group-id $SEC_GROUP_ID \
+--protocol tcp \
+--port 8080 \
+--cidr 0.0.0.0/0
+
+aws ec2 authorize-security-group-ingress \
+--group-id $SEC_GROUP_ID \
 --ip-permissions IpProtocol=tcp,FromPort=2049,ToPort=2049,IpRanges='[{CidrIp=0.0.0.0/0,Description="NFS"}]'
 
 # aws ec2 authorize-security-group-ingress \
