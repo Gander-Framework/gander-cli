283ebfcbea077b96c1c4da99b213cc1fd7cc9be2
ChristopherDurand <chris@cdurand.net>
2021-04-24 15:17:50 -0500

    fix path on fleet template paths

--- src/util/paths.js
+++ src/util/paths.js
@@ -2,16 +2,14 @@ const path = require('path')
 
 const fleetRootPath = path.join(__dirname, '/../..') // ends up at root level of fleet-cli
 
-const fleetBuildWorkflowTemplatePath = path.join(fleetRootPath, '/.build-review-app.yml')
+const fleetBuildWorkflowTemplatePath = path.join(fleetRootPath, '/templates/github/workflows/build-review-app.yml')
 const userBuildWorkflowPath = path.join(process.cwd(), '/.github/workflows/build-review-app.yml')
 
-const fleetTeardownWorkflowPath = path.join(fleetRootPath, '/templates/teardown-review-app.yml')
+const fleetTeardownWorkflowPath = path.join(fleetRootPath, '/templates/github/workflows/teardown-review-app.yml')
 const userTeardownWorkflowPath = path.join(process.cwd(), '/.github/workflows/teardown-review-app.yml')
 
 const githubFolderPath = path.join(process.cwd(), '/.github')
 const workflowFolderPath = path.join(process.cwd(), '/.github/workflows')
-
-const taskExecutionPolicyPath = path.resolve()
 
 const actionFolderPaths = [
   path.join(process.cwd(), "/.github/actions"),
@@ -23,13 +21,15 @@ const actionFolderPaths = [
 ]
 
 const fleetActionTemplatePaths = [
-  path.join(process.cwd(), '/templates/github/actions/build-server/action.yml'),
-  path.join(process.cwd(), '/templates/github/actions/cleanup-efs/action.yml'),
-  path.join(process.cwd(), '/templates/github/actions/clear-efs/action.yml'),
-  path.join(process.cwd(), '/templates/github/actions/launch-review-app/action.yml'),
-  path.join(process.cwd(), '/templates/github/actions/seed-db/action.yml'),
+  path.join(fleetRootPath, '/templates/github/actions/build-server/action.yml'),
+  path.join(fleetRootPath, '/templates/github/actions/cleanup-efs/action.yml'),
+  path.join(fleetRootPath, '/templates/github/actions/clear-efs/action.yml'),
+  path.join(fleetRootPath, '/templates/github/actions/launch-review-app/action.yml'),
+  path.join(fleetRootPath, '/templates/github/actions/seed-db/action.yml'),
 ]
-const userActionPaths = actionFolderPaths.map(path => `${path}/action.yml`)
+const userActionPaths = actionFolderPaths.slice(1).map(path => `${path}/action.yml`)
+
+const taskExecutionPolicyPath = path.resolve()
 
 module.exports = {
   fleetRootPath,
