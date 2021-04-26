e7ad66584af4a9daa08d5476e078497711f174e7
Melinda <melinda.hl.lim@gmail.com>
2021-04-26 12:01:06 -0400

    Refactor iam client to accept input region from user

--- src/aws/api/iam.js
+++ src/aws/api/iam.js
@@ -7,54 +7,54 @@ const {
   AttachRolePolicyCommand,
 } = require('@aws-sdk/client-iam');
 
+const client = require('./client.js');
 const executeProcess = require('./executeSdkProcess.js');
-const { loadJSON } = require('../../util');
+const ganderUtils = require('../../util');
 
-// TODO: pull region from config file
-const AWS_REGION = 'us-east-1';
-
-const client = new IAMClient({
-  region: AWS_REGION,
-});
+const initializeClient = region => {
+  return new IAMClient({
+    region,
+  });
+};
 
 const createGroup = async ({ GroupName }) => executeProcess({
   startMsg: 'Creating IAM group',
   successMsg: 'IAM group successfully created',
-  client,
+  client: client.iam,
   command: new CreateGroupCommand({ GroupName }),
 });
 
 const createUser = async ({ UserName }) => executeProcess({
   startMsg: 'Creating IAM user',
   successMsg: 'IAM user successfully created',
-  client,
+  client: client.iam,
   command: new CreateUserCommand({ UserName }),
 });
 
 const createPolicy = async ({ PolicyName, PolicyPath }) => executeProcess({
   startMsg: 'Creating task execution policy',
   successMsg: 'Task execution policy successfully created',
-  client,
+  client: client.iam,
   command: new CreatePolicyCommand({
     PolicyName,
-    PolicyDocument: loadJSON(PolicyPath),
+    PolicyDocument: ganderUtils.loadJson(PolicyPath),
   }),
 });
 
 const createRole = async ({ RoleName, TrustPath }) => executeProcess({
   startMsg: 'Creating task execution role',
   successMsg: 'Task execution role successfully created',
-  client,
+  client: client.iam,
   command: new CreateRoleCommand({
     RoleName,
-    AssumeRolePolicyDocument: loadJSON(TrustPath),
+    AssumeRolePolicyDocument: ganderUtils.loadJson(TrustPath),
   }),
 });
 
 const attachRolePolicy = async ({ RoleName, PolicyArn }) => executeProcess({
   startMsg: 'Attaching task execution policy to role',
   successMsg: 'Task execution policy successfully attached',
-  client,
+  client: client.iam,
   command: new AttachRolePolicyCommand({
     RoleName,
     PolicyArn,
@@ -62,6 +62,7 @@ const attachRolePolicy = async ({ RoleName, PolicyArn }) => executeProcess({
 });
 
 module.exports = {
+  initializeClient,
   createGroup,
   createUser,
   createPolicy,
