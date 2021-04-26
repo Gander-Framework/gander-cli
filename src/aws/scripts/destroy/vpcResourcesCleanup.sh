de357a0522b6a2ca652b4ce8850a9e3c977d2563
sjbabadi <sheila.babadi@yahoo.com>
2021-04-26 17:57:14 -0400

    add security group deletion to VPC cleanup

--- src/aws/scripts/destroy/vpcResourcesCleanup.sh
+++ src/aws/scripts/destroy/vpcResourcesCleanup.sh
@@ -17,5 +17,8 @@ aws ec2 delete-internet-gateway \
 
 # must first terminate all running instances in the subnet
 aws ec2 delete-subnet \
---subnet-id $SUBNET_ID
+--subnet-id $CLUSTER_SUBNET_ID
 
+aws ec2 delete-security-group --group-id $CLUSTER_SECURITY_GROUP
+
+aws ec2 delete-security-group --group-id $EFS_SECURITY_GROUP
