eee87ad147d5fa2e02b2e4b76f2814edf97b6d94
Melinda <melinda.hl.lim@gmail.com>
2021-04-26 16:28:46 -0400

    Convert creating ecr repo from cli to sdk

--- src/aws/api/index.js
+++ src/aws/api/index.js
@@ -2,6 +2,7 @@ const client = require('./client');
 const iam = require('./iam');
 const ec2 = require('./ec2');
 const efs = require('./efs');
+const ecr = require('./ecr');
 const createEcrRepository = require('./ecrCreateRepository.js');
 const createCluster = require('./ecsCreateCluster.js');
 
@@ -10,6 +11,7 @@ const api = {
   iam,
   ec2,
   efs,
+  ecr,
   createEcrRepository,
   createCluster,
 };
