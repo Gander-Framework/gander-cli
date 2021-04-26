f8ec94023947f6e3b5478eadb06c9034eed22129
sjbabadi <sheila.babadi@yahoo.com>
2021-04-26 18:11:14 -0400

    fix spinner declaration in destroy.js

--- src/commands/destroy.js
+++ src/commands/destroy.js
@@ -2,6 +2,7 @@ const {Command} =  require('@oclif/command')
 const prompts = require('../prompts')
 const api = require('../aws/api')
 const utils = require('../util')
+const log = require('../util/log')
 
 
 class DestroyCommand extends Command {
@@ -17,26 +18,29 @@ class DestroyCommand extends Command {
 
 
     let mountTargetDeleted = false;
-    const pollSpinner
+    const pollSpinner = log.spin('Deleting Mount Target')
     while(!mountTargetDeleted) {
       utils.sleep(500)
       const describeMountTargetsResponse = await api.describeMountTargets()
       const mtLength = JSON.parse(describeMountTargetsResponse).MountTargets.length
       mountTargetDeleted = mtLength === 0 ? true : false
     }
+    pollSpinner.succeed('Mount Target deleted')
 
     
     
     // TODO: delete EFS
     await api.efsDelete()
 
+    // TODO: delete ECR repo
+    await api.ecrRepoDelete()
+
     // TODO: delete VPC resources
     await api.vpcResourcesCleanup()
     // TODO: delete VPC
     await api.vpcDelete()
     // TODO: delete cluster
-    // TODO: delete ECR repo
-    await api.ecrRepoDelete()
+
     
   }
 }
