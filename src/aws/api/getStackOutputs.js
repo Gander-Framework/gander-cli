3e43e95ea23240723e333d8d60fee574a696c562
ChristopherDurand <chris@cdurand.net>
2021-04-27 17:10:37 -0500

    Generate Stack
    Co-authored-by: Melinda Lim <melinda.hl.lim@gmail.com>
    Co-authored-by: Andrew Jones <andrewrayjones@gmail.com>
    Co-authored-by: Sheila Babadi <sheila.babadi@yahoo.com>

---
+++ src/aws/api/getStackOutputs.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line unicorn/filename-case
+const path = require('path')
+const executeProcess = require('./executeProcess.js')
+
+const getStackOutputs = async (stackName) => {
+  return executeProcess(
+    'Getting Outputs of the Stack',
+    'Outputs Retrieved',
+    () => {
+      const arg1 = `STACK_NAME=${stackName}`
+      const script = path.resolve(__dirname, '../scripts/getStackOutputs.sh')
+
+      return `${arg1} ${script}`
+    }
+  )
+}
+
+module.exports = setupEfsSgIngress
--- src/commands/setup-cf.js
+++ src/commands/setup-cf.js
@@ -1,13 +1,20 @@
 const { Command } = require('@oclif/command');
 const Conf = require('conf');
+const api = require('../aws/api');
 const prompts = require('../prompts');
+const paths = require('../utils/paths.js');
+
 const config = new Conf();
 
 class SetupCFCommand extends Command {
   async run() {
     const initialConfig = await prompts.welcome();
     config.set('AWS_REGION', initialConfig.awsRegion);
-    
+
+    console.log('\nGenerating your Fleet infrastructure. This may take a few minutes, so grab some coffee~ \n');
+    api.createStack('fleet-apps',paths.cloudFormationTemplatePath,initialConfig.awsRegion)
+
+    api.getStackOutputs('fleet-apps')
   }
 }
 
