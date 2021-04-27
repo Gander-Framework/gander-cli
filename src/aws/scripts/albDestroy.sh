e48f3958de3e271f79d59a0f1820e1380cfeb302
Andrew Jones <andrewrayjones@gmail.com>
2021-04-27 10:53:30 -0500

    test destroy with listener and ALB already deleted
    Co-authored-by: Melinda Lim <melinda.hl.lim@gmail.com>
    Co-authored-by: Sheila Babadi <sheila.babadi@yahoo.com>
    Co-authored-by: Chris Durand <chris@cdurand.net>

--- src/aws/scripts/albDestroy.sh
+++ src/aws/scripts/albDestroy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-aws elbv2 delete-listener --listener-arn $ALB_LISTENER_ARN
+# aws elbv2 delete-listener --listener-arn $ALB_LISTENER_ARN
 
-aws elbv2 delete-load-balancer --load-balancer-arn $ALB_LB_ARN
+# aws elbv2 delete-load-balancer --load-balancer-arn $ALB_LB_ARN
 
 aws ec2 delete-subnet --subnet-id $ALB_SUBNETA_ID
 aws ec2 delete-subnet --subnet-id $ALB_SUBNETB_ID
