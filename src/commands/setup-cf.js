c45bdc067eacb77e81be5c63e2457327736459cb
ChristopherDurand <chris@cdurand.net>
2021-04-27 18:17:29 -0500

    manually add configurations from outputs

--- src/commands/setup-cf.js
+++ src/commands/setup-cf.js
@@ -6,6 +6,8 @@ const paths = require('../util/paths.js');
 
 const config = new Conf();
 
+const DEFAULT_NAME = 'fleet-apps';
+
 class SetupCFCommand extends Command {
   async run() {
     const initialConfig = await prompts.welcome();
@@ -13,17 +15,29 @@ class SetupCFCommand extends Command {
 
     console.log('\nGenerating your Fleet infrastructure. This may take a few minutes, so grab some coffee~ \n');
     await api.createStack('fleet-apps',paths.cloudFormationTemplatePath,initialConfig.awsRegion)
-    const outputs = JSON.parse(await api.getStackOutputs('fleet-apps'))
-    outputs.forEach(({OutputKey, OutputValue}) => {
-      config.set(OutputKey,OutputValue)
-    })
-    console.log('It may take around 10 minutes for AWS to fully spin up all infrastructure pieces. \nBut for now, we\'re all done! :D')
+    const rawOutputs = JSON.parse(await api.getStackOutputs('fleet-apps'))
+    let outputs = {};
+    rawOutputs.forEach(output => outputs[output.OutputKey] = output.OutputValue)
     
-    config.set('DEFAULT_SUBNET_NAME', DEFAULT_NAME)
-    config.set('CLUSTER_SECURITY_GROUP', `${DEFAULT_NAME}-cluster`)
-    config.set('EFS_CREATION_TOKEN', DEFAULT_NAME)
-    config.set('EFS_NAME', DEFAULT_NAME)
-    config.set('APP_NAMES', "[]")
+    console.log('It may take around 10 minutes for AWS to fully spin up all infrastructure pieces. \nBut for now, we\'re all done! :D')
+    config.set('VPC_ID', outputs.VPCID);
+    config.set('CLUSTER_SECURITY_GROUP_ID', outputs.ClusterSecurityGroupID);
+    config.set('CLUSTER_SUBNET_ID', outputs.ClusterSubnetID);
+    config.set('ALB_SECURITY_GROUP_ID', outputs.ALBSecurityGroupID);
+    config.set('CLUSTER_SUBNETA_ID', outputs.ALBSubnetAID);
+    config.set('CLUSTER_SUBNETB_ID', outputs.ALBSubnetAID);
+    config.set('ROUTE_TABLE_ID', outputs.RouteTableID);
+    config.set('ALB_ARN', outputs.ALBARN);
+    config.set('LISTENER_ARN', outputs.ListenerArn);
+    config.set('ALB_DOMAIN', outputs.ALBDomain);
+    config.set('EFS_SECURITY_GROUP_ID', outputs.EFSSecurityGroupID);
+    config.set('EFS_ID', outputs.EFSID);
+    config.set('MOUNT_TARGET_ID', outputs.MountTargetID);
+    config.set('DEFAULT_SUBNET_NAME', DEFAULT_NAME);
+    config.set('CLUSTER_SECURITY_GROUP', `${DEFAULT_NAME}-cluster`);
+    config.set('EFS_CREATION_TOKEN', DEFAULT_NAME);
+    config.set('EFS_NAME', DEFAULT_NAME);
+    config.set('APP_NAMES', "[]");
   }
 }
 
