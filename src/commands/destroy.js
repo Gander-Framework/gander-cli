d295a1df27496713675e8630eaaa30a75f00c136
Andrew Jones <andrewrayjones@gmail.com>
2021-04-27 11:22:23 -0500

    long poll after ALB deletion until subnet deletion OK
    Co-authored-by: Melinda Lim <melinda.hl.lim@gmail.com>
    Co-authored-by: Sheila Babadi <sheila.babadi@yahoo.com>
    Co-authored-by: Chris Durand <chris@cdurand.net>

--- src/commands/destroy.js
+++ src/commands/destroy.js
@@ -1,9 +1,23 @@
 const {Command} =  require('@oclif/command')
 const api = require('../aws/api')
+const log = require('../util/log')
+const utils = require('../util')
 
 class DestroyCommand extends Command {
   async run() {
     await api.destroyAlb()
+
+    let albDeleted = false
+    const pollSpinner = log.spin('Deleting ALB and listener')
+    while (!albDeleted) {
+      utils.sleep(500)
+      const describeLoadBalancersResponse = await api.describeLoadBalancers()
+      const lbLength = JSON.parse(describeLoadBalancersResponse).LoadBalancerDescriptions.length
+      albDeleted = lbLength === 0
+    }
+    pollSpinner.succeed('ALB and listener deleted')
+
+    await api.destroyAlbSubnetsSg()
   }
 }
 
