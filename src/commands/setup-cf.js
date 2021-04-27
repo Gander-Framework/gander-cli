70828a278b80daeac26d738794b742440a9386bf
ChristopherDurand <chris@cdurand.net>
2021-04-27 18:04:04 -0500

    set outputs to the config file
    Co-authored-by: Melinda Lim <melinda.hl.lim@gmail.com>
    Co-authored-by: Andrew Jones <andrewrayjones@gmail.com>
    Co-authored-by: Sheila Babadi <sheila.babadi@yahoo.com>

--- src/commands/setup-cf.js
+++ src/commands/setup-cf.js
@@ -14,8 +14,16 @@ class SetupCFCommand extends Command {
     console.log('\nGenerating your Fleet infrastructure. This may take a few minutes, so grab some coffee~ \n');
     await api.createStack('fleet-apps',paths.cloudFormationTemplatePath,initialConfig.awsRegion)
     const outputs = JSON.parse(await api.getStackOutputs('fleet-apps'))
-    console.log(outputs)
+    outputs.forEach(({OutputKey, OutputValue}) => {
+      config.set(OutputKey,OutputValue)
+    })
+    console.log('It may take around 10 minutes for AWS to fully spin up all infrastructure pieces. \nBut for now, we\'re all done! :D')
     
+    config.set('DEFAULT_SUBNET_NAME', DEFAULT_NAME)
+    config.set('CLUSTER_SECURITY_GROUP', `${DEFAULT_NAME}-cluster`)
+    config.set('EFS_CREATION_TOKEN', DEFAULT_NAME)
+    config.set('EFS_NAME', DEFAULT_NAME)
+    config.set('APP_NAMES', "[]")
   }
 }
 
