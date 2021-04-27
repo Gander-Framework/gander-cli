cbdbd46fd1d5af5ba8abe281593b36685854bd0d
Andrew Jones <andrewrayjones@gmail.com>
2021-04-27 12:16:32 -0500

    handle empty describe-load-balancers response
    Co-authored-by: Melinda Lim <melinda.hl.lim@gmail.com>
    Co-authored-by: Sheila Babadi <sheila.babadi@yahoo.com>
    Co-authored-by: Chris Durand <chris@cdurand.net>

--- src/aws/api/albDestroy.js
+++ src/aws/api/albDestroy.js
@@ -7,7 +7,7 @@ const config = new Conf()
 const destroyAlb = async () => {
   return executeProcess(
     'Destroying application load balancer and listener',
-    'ALB and listener deleted',
+    'ALB and listener delete command executed',
     () => {
       const script = path.resolve(__dirname, '../scripts/albDestroy.sh')
       const arg1 = `ALB_LISTENER_ARN=${config.get('LISTENER_ARN')}`
