90b4a243a05dfb17ecd973412cda6c5f9279e14c
Melinda <melinda.hl.lim@gmail.com>
2021-04-28 10:52:35 -0400

    Replace command-related instances of 'fleet' with 'gander'; remove some unused imports in the command files

--- src/commands/destroy.js
+++ src/commands/destroy.js
@@ -1,27 +1,22 @@
-const {Command} =  require('@oclif/command')
-const prompts = require('../prompts')
-const api = require('../aws/api')
-const utils = require('../util')
-const log = require('../util/log')
-const Conf = require('conf')
-const config = new Conf()
-
+const { Command } =  require('@oclif/command');
+const api = require('../aws/api');
+const Conf = require('conf');
+const config = new Conf();
 
 const destroyAllClusters = () => {
-  const appNames = JSON.parse(config.get('APP_NAMES'))
+  const appNames = JSON.parse(config.get('APP_NAMES'));
   appNames.forEach(name => {
-    // TODO: call api.deleteCluster
-    api.deleteCluster(name)
-  })
-}
+    api.deleteCluster(name);
+  });
+};
 
 class DestroyCommandCF extends Command {
   async run() {
-    await api.destroyStack('fleet-apps')
-    destroyAllClusters()
+    await api.destroyStack('fleet-apps');
+    destroyAllClusters();
   }
 }
 
-DestroyCommandCF.description = 'Teardown all of the Fleet infrastructure that was created during initial setup using CloudFormation'
+DestroyCommandCF.description = 'Destroy all AWS resources created for Gander review apps';
 
-module.exports = DestroyCommandCF
+module.exports = DestroyCommandCF;
--- src/commands/init.js
+++ src/commands/init.js
@@ -1,37 +1,36 @@
-const {Command} =  require('@oclif/command')
-const prompts = require('../prompts')
-const api = require('../aws/api')
-const Conf = require('conf')
-const config = new Conf()
-const {fleetRootPath, fleetBuildWorkflowTemplatePath} = require('../util/paths')
+const { Command } =  require('@oclif/command');
+const prompts = require('../prompts');
+const api = require('../aws/api');
+const Conf = require('conf');
+const config = new Conf();
 const {
   populateWorkflows,
   createWorkflowDir,
   copyWorkflowFilesToRepo,
-} = require('../util/fs')
+} = require('../util/fs');
 
 const getAppInfo = async () => {
-  const appInfo = await prompts.appInfoPrompt()
-  return appInfo
-}
+  const appInfo = await prompts.appInfoPrompt();
+  return appInfo;
+};
 
 const addAppToConfigFile = APP_NAME => {
-  let apps = JSON.parse(config.get('APP_NAMES'))
-  apps.push(APP_NAME)
-  config.set('APP_NAMES', JSON.stringify(apps))
-}
+  let apps = JSON.parse(config.get('APP_NAMES'));
+  apps.push(APP_NAME);
+  config.set('APP_NAMES', JSON.stringify(apps));
+};
 
 class InitCommand extends Command {
   async run() {
-    createWorkflowDir()
-    copyWorkflowFilesToRepo()
-    const appInfo = await getAppInfo()
-    addAppToConfigFile(appInfo.APP_NAME)
-    await api.createCluster(appInfo.APP_NAME)
-    await populateWorkflows(appInfo)
+    createWorkflowDir();
+    copyWorkflowFilesToRepo();
+    const appInfo = await getAppInfo();
+    addAppToConfigFile(appInfo.APP_NAME);
+    await api.createCluster(appInfo.APP_NAME);
+    await populateWorkflows(appInfo);
   }
 }
 
-InitCommand.description = 'Initialize a Fleet App in your project repository'
+InitCommand.description = 'Initialize your project repository as a Gander review app';
 
-module.exports = InitCommand
+module.exports = InitCommand;
