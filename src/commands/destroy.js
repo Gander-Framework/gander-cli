54cc562a8d85bd9c90066de191e6aae4fde33fc1
Melinda <melinda.hl.lim@gmail.com>
2021-04-28 15:05:22 -0400

    Convert destroy api calls from cli to sdk - untested

--- src/commands/destroy.js
+++ src/commands/destroy.js
@@ -3,6 +3,8 @@ const api = require('../aws/api');
 const Conf = require('conf');
 const config = new Conf();
 
+const DEFAULT_NAME = 'gander-apps';
+
 const destroyAllClusters = () => {
   const appNames = JSON.parse(config.get('APP_NAMES'));
   appNames.forEach(name => {
@@ -12,7 +14,11 @@ const destroyAllClusters = () => {
 
 class DestroyCommandCF extends Command {
   async run() {
-    await api.destroyStack('gander-apps');
+    api.clients.cloudFormation = await api.initializeCfClient(config.get('AWS_REGION'));
+
+    api.clients.ecs = await api.initializeEcsClient(config.get('AWS_REGION'));
+
+    await api.deleteStack({ StackName: DEFAULT_NAME });
     destroyAllClusters();
   }
 }
