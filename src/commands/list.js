b27abdedfba3fc0250b98792167d85b9915cb49b
Andrew Jones <andrewrayjones@gmail.com>
2021-04-29 14:37:48 -0500

    add list command to list currently active apps

--- src/commands/list.js
+++ src/commands/list.js
@@ -1,8 +1,13 @@
 const { Command } =  require('@oclif/command');
+const Conf = require('conf');
+const config = new Conf();
+const log = require('../util/log');
 
 class ListCommand extends Command {
   async run() {
-    
+    const appNames = JSON.parse(config.get('APP_NAMES'));
+    log.text('Your current Gander apps:');
+    appNames.forEach(appName => log.info(appName));
   }
 }
 
