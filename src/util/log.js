1c268bd1e63dfab2ca39d7bd21f3f189f2f1ba63
ChristopherDurand <chris@cdurand.net>
2021-05-04 18:45:44 -0500

    add logo
    Co-authored-by: Melinda Lim <melinda.hl.lim@gmail.com>
    Co-authored-by: Andrew Jones <andrewrayjones@gmail.com>
    Co-authored-by: Sheila Babadi <sheila.babadi@yahoo.com>

--- src/util/log.js
+++ src/util/log.js
@@ -1,11 +1,18 @@
 const ora = require('ora')
 const chalk = require('chalk')
-
+const { LOGO, COLOR_MAP } = require('./logo')
 const primary = chalk.keyword('orange').bold
 const secondary = chalk.cyan
 const neutral = chalk.white
 const emphasis = chalk.bold.red
 const success = chalk.green
+
+const logoColorMap = {
+  o: char => chalk.hex("#ef6f56")(char),
+  w: char => chalk.hex("#ffffff")(char),
+  g: char => chalk.hex("#89d8c5")(char),
+  [' ']: char => char
+}
 
 module.exports = {
   spin(msg, opts) {
@@ -13,7 +20,32 @@ module.exports = {
     spinner.color = opts?.color || 'cyan'
     return spinner.start()
   },
-
+  printLogo() {
+    let logo = '';
+    for (let i = 0; i < LOGO.length; i++) {
+      const char = LOGO[i];
+      let candidate;
+      switch (COLOR_MAP[i]) {
+        case 'o':
+          candidate = chalk.hex("#ef6f56")(char);
+          break;
+        case 'g':
+          candidate = chalk.hex("#89d8c5")(char);
+          break;
+        case 'w':
+          candidate = chalk.hex("#ffffff")(char);
+          break;
+        default:
+          candidate = char;
+          break;
+      }
+      if(candidate === 'undefined') {
+        console.log(i, LOGO[i], COLOR_MAP[i])
+      }
+      logo += candidate;
+    }
+    console.log(logo);
+  },
   header(msg) {
     console.log(primary(msg));
   },
---
+++ src/util/logo.js
@@ -0,0 +1,57 @@
+const LOGO = `                       *%&@@@@@@@@@@@@@@@&%*
+                 ,&@@@@%/,,,,,,,,,,,,,*,,**%@@@@&*
+             ,&@@&(,,,,,,,,,,,,,,,,,,,,,,,,,,,,,/&@@&*
+          *&@@#,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,(@@@*
+        (@@%,,,,,,,,,,,,,,,,,,,,,,,,,*(((/,,,,,,,,,,,,,#@@%
+      #@@(,,,,,,,,,,,,,,,,,,,/(#%%&@@@@@@@@@@&%%%%%&&%%%%&@@&%%%%%%%%%%(.
+    ,@@#,,,,,,,,,,,,,,,,#@@@@&(/,,....(@@*........,@@(/////((/////////(%@@.
+   #@@*,,,,,,,,,,,,,*%&@&,     ,#@@@@@@&          ,@@(////////////////(&@@.
+  #@&*,,,,,,,,,,,,,&&&,      &@@#.    (&@&.       ,@@(/////////////%@@@%.
+ /@&*,,,,,,,,,,,,/@@/      .@@/   .**   ,@@,      ,@@(/////////#@@@%.
+.&@/,,,,,,,,,,,,(@@,       /@@   (@@@@.  @@#      ,@@(/////(&@@&@@.
+/@&,,,,,,,,,,,,,&@(         &@#         /@@.      ,@@(//&@@@#,,,&@#
+%@&,,,,,,,,,,,,*@@*        (@@@@@#. .(&@@#        ,@@@@@%*,,,,,,#@&
+&@%,,,,,,,,,,,,,@@/      %@@/   ./#&%(.         (&@@&*,,,,,,,,,,#@&
+#@&,,,,,,,,,,,,,(@&    %@@*                 *&@@&/,,,,,,,,,,,,,,%@&
+,@@*,,,,,,,,,,,,,%@&.&@&,               ,%@@@(,,,,,,,,,,,,,,,,,*&@*
+ %@&,,,,,,,,,,,,,,&@@&.               #@@%*,,,,,,,,,,,,,,,,,,,,%@&
+  &@#,,,,,,,,,,,#@@@@#               *@@*,,,,,,,,,,,,,,,,,,,,,(@&
+  .&@%,,,,,,,,,%@&,*@@(               %@&,,,,,,,,,,,,,,,,,,,,#@&.
+    %@@*,,,,,,,,#%,,/@@,               &@%,,,,,,,,,,,,,,,,,*&@&
+     ,&@%,,,,,,,,,,,,(@&.              .@@#,,,,,,,,,,,,,,,#&@/
+       /@@%,,,,,,,,,,,#@&               ,@@/,,,,,,,,,,,,%@@(
+         ,@@@(,,,,,,,,,&@@               *@@*,,,,,,,,/@@@,
+            /&@&%,,,,,,,&@#               (@@*,,,,%@@@(
+               .#@@@&#*,*@@/               %@@&@@@%.
+                    ,#&@@@@@&%%%#(#%%&&@@@@@&%,
+                             ..,,,,...`
+
+const COLOR_MAP = `                       wwwwwwwwwwwwwwwwwwwww
+                 wwwwwwwwggggggggggggggggwwwwwwwww
+             wwwwwwgggggggggggggggggggggggggggggwwwwww
+          wwwwwgggggggggggggggggggggggggggggggggggggwwwww
+        wwwwggggggggggggggggggggggggggggggggggggggggggwwwww
+      wwwwgggggggggggggggggggwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+    wwwwggggggggggggggggwwwwwwwwwwwwwwwwwwwwwwwwwwwwwoooooooooooooooooowwww
+   wwwwgggggggggggggwwwwww     wwwwwwwww          wwwoooooooooooooooooowwww
+  wwwwgggggggggggggwwww      wwwww    wwwww       wwwoooooooooooooowwwwww
+ wwwwggggggggggggwwww      wwww   www   wwww      wwwoooooooooowwwwww
+wwwwggggggggggggwwww       www   wwwwww  www      wwwooooooowwwwwww
+wwwgggggggggggggwww         www         wwww      wwwooowwwwwgggwww
+wwwggggggggggggwwww        wwwwwwww wwwwww        wwwwwwwwggggggwww
+wwwgggggggggggggwww      wwww   wwwwwww         wwwwwwggggggggggwww
+wwwgggggggggggggwww    wwww                 wwwwwwggggggggggggggwww
+wwwwgggggggggggggwwwwwwww               wwwwwwgggggggggggggggggwwww
+ wwwggggggggggggggwwwww               wwwwwggggggggggggggggggggwww
+  wwwgggggggggggwwwwww               wwwwgggggggggggggggggggggwww
+  wwwwgggggggggwwwgwwww               wwwggggggggggggggggggggwwww
+    wwwwggggggggwwggwwww               wwwgggggggggggggggggwwww
+     wwwwggggggggggggwwww              wwwwgggggggggggggggwwww
+       wwwwgggggggggggwww               wwwwggggggggggggwwww
+         wwwwwgggggggggwww               wwwwggggggggwwwww
+            wwwwwgggggggwww               wwwwggggwwwww
+               wwwwwwwwgwwww               wwwwwwwww
+                    wwwwwwwwwwwwwwwwwwwwwwwwwww
+                             wwwwwwwww`
+
+module.exports = { LOGO, COLOR_MAP }
